3d3a92d08812f650cb9556ada21080466a7406bb
Wojciech Pryszcz <wojciech.pryszcz@wp.pl>
2023-09-06 14:14:58 +0200

    Curson animation
    Cursor animation in Vanilla JS. I made it from tutorial https://www.youtube.com/watch?v=BfWZmBzVxlw&list=PL9bD98LkBR7P16BndaNtP4x6Wf5Ib85Hm&index=7

---
+++ script.js
@@ -0,0 +1,22 @@
+const cursor = document.querySelector(".cursor");
+let timeout;
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  const mouseStopped = (params) => {
+    cursor.style.display = "none";
+  };
+
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 1000);
+});
+
+document.addEventListener("mouseout", () => {
+  cursor.style.display = "none";
+});
